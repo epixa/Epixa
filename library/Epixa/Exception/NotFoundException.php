06db213e6de63976e8e01dd8d67612e60165646a
Court Ewing <court@epixa.com>
2010-09-25 18:52:57 -0400

    Added a NotFoundException

---
+++ library/Epixa/Exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Epixa Library
+ */
+
+namespace Epixa\Exception;
+
+/**
+ * @category  Epixa
+ * @copyright 2010 epixa.com - Court Ewing
+ * @license   http://github.com/epixa/Epixa/blob/master/LICENSE New BSD
+ * @author    Court Ewing (court@epixa.com)
+ */
+class NotFoundException extends \RuntimeException
+{}
