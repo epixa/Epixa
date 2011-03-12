75ef66df3d816daffc42912d7ab3a06c16b94fbd
Court Ewing <court@epixa.com>
2011-03-11 22:17:59 -0500

    Updated email service with a send method as well as a requirement to inject a view object for email template rendering

--- library/Epixa/Service/AbstractEmailService.php
+++ library/Epixa/Service/AbstractEmailService.php
@@ -6,6 +6,7 @@
 namespace Epixa\Service;
 
 use Zend_Mail as Mailer,
+    Zend_View_Interface as View,
     Epixa\Exception\ConfigException;
 
 /**
@@ -54,12 +55,37 @@ abstract class AbstractEmailService
 
         return self::$defaultMailer;
     }
+    
+    /**
+     * Sets the default view object for email services
+     *
+     * @param View $view
+     */
+    public static function setDefaultView(View $view)
+    {
+        self::$defaultView = $view;
+    }
 
+    /**
+     * Gets the default view object for email services
+     *
+     * @return View
+     * @throws ConfigException If no default view is set
+     */
+    public static function getDefaultView()
+    {
+        if (self::$defaultView === null) {
+            throw new ConfigException('No default view configured');
+        }
+
+        return self::$defaultView;
+    }
+    
     /**
      * Sets the mailer for this service
      *
      * @param  Mailer $mailer
-     * @return AbstractService *Fluent interface*
+     * @return AbstractEmailService *Fluent interface*
      */
     public function setMailer(Mailer $mailer)
     {
@@ -82,5 +108,86 @@ abstract class AbstractEmailService
         }
 
         return $this->mailer;
+    }
+
+    /**
+     * Sets the view for this service
+     *
+     * @param  View $view
+     * @return AbstractEmailService *Fluent interface*
+     */
+    public function setView(View $view)
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+    /**
+     * Gets the view for this service
+     *
+     * If no view is set, sets it to the default view.
+     *
+     * @return View
+     */
+    public function getView()
+    {
+        if ($this->view === null) {
+            $this->setView(self::getDefaultView());
+        }
+
+        return $this->view;
+    }
+    
+    /**
+     * Renders the given email template and sends it to the given email
+     * 
+     * @param string               $template
+     * @param string               $email
+     * @param string               $name
+     * @param string               $subject
+     * @param null|\Zend_Mime_Part $attachment
+     */
+    public function send($template, $email, $name, $subject, $attachment = null)
+    {
+        $view = $this->getView();
+        
+        $html = $view->render($this->_getHtmlTemplateFile($template));
+        $text = $view->render($this->_getTextTemplateFile($template));
+        
+        $mail = $this->getMailer();
+        $mail->addTo($email, $name)
+             ->setSubject($subject)
+             ->setBodyHtml($html)
+             ->setBodyText($text);
+        
+        if ($attachment !== null) {
+            $mail->addAttachment($attachment);
+        }
+        
+        $mail->send();
+    }
+    
+    
+    /**
+     * Creates the path to the html version of the given template file
+     * 
+     * @param  string $template
+     * @return string
+     */
+    protected function _getHtmlTemplateFile($template)
+    {
+        return $template . '-html.phtml';
+    }
+    
+    /**
+     * Creates the path to the text version of the given template file
+     * 
+     * @param  string $template
+     * @return string
+     */
+    protected function _getTextTemplateFile($template)
+    {
+        return $template . '-text.phtml';
     }
 }
