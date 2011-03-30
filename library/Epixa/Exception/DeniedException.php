072c0e6290925659faa6aa000b3f43ba91ac6f2b
Court Ewing <court@epixa.com>
2011-03-30 16:12:52 -0400

    Added exception denoting a denied request

---
+++ library/Epixa/Exception/DeniedException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Epixa Library
+ */
+
+namespace Epixa\Exception;
+
+use LogicException;
+
+/**
+ * @category  Epixa
+ * @package   Exception
+ * @copyright 2011 epixa.com - Court Ewing
+ * @license   http://github.com/epixa/Epixa/blob/master/LICENSE New BSD
+ * @author    Court Ewing (court@epixa.com)
+ */
+class DeniedException extends LogicException
+{}
